ffb0e3c5de5135f7aa2978e2e3d208e2e56fbcf1
Anugera Rejeki Abadi <martin12082000@gmail.com>
2025-05-24 02:30:35 +0700

    belajar class tombol

--- src/app/inventaris/page.tsx
+++ src/app/inventaris/page.tsx
@@ -5,13 +5,21 @@ const InventarisPage = () => {
   return (
     <div>
 
-      <button className="w-[10rem] bg-green-500 text-white text-[2rem] rounded-[15px]">
-          <h1>Klik Saya</h1>
-      </button>
+      <div id="element induk" className=" flex flex-col gap-2">
 
-      <button className="w-[8rem] bg-blue-300 test  white text-[15px] rounded-[1rem]">
-      <h1>Peekaboo</h1>
-      </button>
+        <button className="w-[10rem] bg-green-500 text-white text-[2rem] rounded-[15px] hover:bg-yellow-500">
+            <h1>Klik Saya</h1>
+        </button>
+
+        <button className="w-[8rem] bg-blue-300 test  white text-[15px] rounded-[1rem]">
+          <h1>Peekaboo</h1>
+        </button>
+
+        <button className="w-[8rem] bg-blue-300 test  white text-[15px] rounded-[1rem]">
+          <h1>Peekaboo</h1>
+        </button>
+
+      </div>
 
       <div className="max-w-8xl mx-auto px-4 py-16 space-y-24">
         <h1>Inventaris</h1>
