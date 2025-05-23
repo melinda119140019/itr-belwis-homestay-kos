a50d0ae08e2ce85d4782b8a37a7e37d550e54f85
Anugera Rejeki Abadi <martin12082000@gmail.com>
2025-05-24 02:05:48 +0700

    belajar buat tombol

--- src/app/inventaris/page.tsx
+++ src/app/inventaris/page.tsx
@@ -4,8 +4,14 @@ import Image from "next/image";
 const InventarisPage = () => {
   return (
     <div>
+
+      <button className="w-[10rem] bg-green-500 text-white text-[2rem] rounded-[15px]">
+          <h1>Klik Saya</h1>
+      </button>
+
+
       <div className="max-w-8xl mx-auto px-4 py-16 space-y-24">
-      <h1>Inventaris</h1>
+        <h1>Inventaris</h1>
       </div>
       <div className="max-w-8xl mx-auto px-4 py-16 space-y-24">
      <h1>Kos Laki-laki</h1>
@@ -13,6 +19,7 @@ const InventarisPage = () => {
      <div className="max-w-8xl mx-auto px-4 py-16 space-y-24">
      <h1>Kos Perempuan</h1>
      </div>
+
     </div>
   )
   
